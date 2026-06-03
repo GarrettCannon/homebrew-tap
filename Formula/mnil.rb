0b8729b14be234eb424a959fa90a6f92bfbc6498
Garrett Cannon <me@garrettcannon.dev>
2026-06-03 10:43:19 -0400

    v0.2.0 bump

--- Formula/mnil.rb
+++ Formula/mnil.rb
@@ -1,8 +1,8 @@
 class Mnil < Formula
     desc "TUI log viewer that follows piped stdin"
     homepage "https://github.com/garrettcannon/mnil"
-    url "https://github.com/garrettcannon/mnil/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "c378ee9403cf9cc78380330a35ffcda0b1718ef513af4c4e71324c8d1dce2d29"
+    url "https://github.com/garrettcannon/mnil/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "1543c6d8ff2b6f91e92b7816e6f90a5b137e44a4d5f05887639cf1434b8a45a4"
     license "MIT"
     head "https://github.com/garrettcannon/mnil.git", branch: "main"
 
